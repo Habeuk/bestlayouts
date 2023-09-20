74b82ea57da1c325dda454dcaf2d7f5fd27e25c7
kouwa stephane <kksasteph888@gmail.com>
2023-09-20 15:17:19 +0200

    update class icon

--- wbu-atomique-theme/src/js/headers/bestlayouts_dynamiques_headers.js
+++ wbu-atomique-theme/src/js/headers/bestlayouts_dynamiques_headers.js
@@ -1,3 +1,3 @@
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/utilitaires/mega-menu/mega-menu.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/utilitaires/mega-menu/mega-menu-drupal.js";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/utilitaires/mega-menu/mega-menu.scss";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/utilitaires/mega-menu/model-marseille.scss";
